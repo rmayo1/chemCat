aa03e11629d91666b283964082d6f53de3f73943
Grayson Wilkins <gwilkin1@wm320-a229569.campus.ithaca.lan>
2015-04-15 20:49:03 -0400

    Test Code
    View and Chemical Tests

--- chemCatTests/chemCatTests.swift
+++ chemCatTests/chemCatTests.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import XCTest
+import chemCat
 
 class chemCatTests: XCTestCase {
     
@@ -31,6 +32,34 @@ class chemCatTests: XCTestCase {
         self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testViewDidLoad() {
+        let v1 = MasterViewController()
+        let v2 = DetailViewController()
+        let v3 = LoginViewController()
+        let v4 = CatalogViewController()
+        let v5 = CatalogDetailViewController()
+        
+        XCTAssertNotNil(v1.view, "MasterView Did Not Load")
+        XCTAssertNotNil(v2.view, "DetailView Did Not Load")
+        XCTAssertNotNil(v3.view, "LoginView Did Not Load")
+        XCTAssertNotNil(v4.view, "CatalogView Did Not Load")
+        XCTAssertNotNil(v5.view, "CatalogDetailView Did Not Load")
+    }
+    
+    func testChemicalModel() {
+        let name = "hydrochloric acid"
+        let barcode = 123
+        let stock = 5
+        let units = "mL"
+        let expDate = 2016
+        let ehs = true
+        
+        Chemical(barcode: barcode, name: name, stock: stock, expDate: expDate, units: units, ehs: ehs)
+        
+        //XCTAssertTrue(<#expression: @autoclosure () -> BooleanType#>, <#message: String#>)
+        
     }
     
 }
