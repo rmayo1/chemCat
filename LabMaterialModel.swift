872066a340bc20889a40f967d75ec287fc75a985
rmayo1 <rmayo1@wm320-a229578.campus.ithaca.lan>
2015-04-30 20:46:05 -0400

    Comment code to LabMaterial

--- LabMaterialModel.swift
+++ LabMaterialModel.swift
@@ -8,13 +8,24 @@
 
 import Foundation
 
+/* Class: LabMaterial
+* Last Modified: 4/30/15
+* Author: Grayson
+* Purpose: Creates and maintains lab material class
+*/
 class LabMaterial {
-    
+    // delcares variables
     var barcode : Int
     var name : String
     var stock : Int
     var userNames : [String]
     
+    /* init
+    * Parameters: integer for barcode, stirng for item name, int for stock quantity, list of stirngs for user name
+    * Last Modified: 4/30/15
+    * Author: Grayson
+    * Purpose: initializes variables of the class
+    */
     init(barcode:Int, name:String, stock:Int, userNames:[String]){
         self.barcode = barcode
         self.name = name
@@ -22,10 +33,23 @@ class LabMaterial {
         self.userNames = []
     }
     
+    /* Function: getName
+    * Parameters: string for name
+    * Last Modified: 4/30/15
+    * Author: Grayson
+    * Purpose: returns name of item
+    */
     func getName() -> String {
         return self.name
     }
     
+    /* Function: getUsers
+    * Parameters: string for userNames
+    * Last Modified: 4/30/15
+    * Author: Grayson
+    * Purpose: returns names of the users
+    */
+
     func getUsers() -> [String] {
         return self.userNames
     }
